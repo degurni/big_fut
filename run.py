11282bacc7c4834f02936875f752e881c242c480
deg <degurni@rambler.ru>
2022-11-10 22:16:26 +0300

    new

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ while True:
     for para in paras:
         poz = ag.get_position(contract=para)  # проверяем открыта ли позиция
         df = bot.create_df(para=para)  # создаём датафрейм с последними свечами и сигналами индикаторов
-        if poz.size == 0 and kol_poz >= conf.max_poz:  # если позиция ещё не открыта ищем вход в сделку
+        if poz.size == 0 and kol_poz < conf.max_poz:  # если позиция ещё не открыта ищем вход в сделку
             bot.debug('debug', '{}: Позиция ещё не открыта'.format(para))
             # print('CCI_1_9 - {} : CCI_1 - {}'.format(df.CCI[-10:-1].mean(), df.CCI[-1]))
             if df.sigCCI[-1] == 1:  # если получен сигнал на LONG
